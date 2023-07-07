37d6f89ca0c1eea89e97f32fbdc4fa249077e187
UnCompa <125362398+UnCompa@users.noreply.github.com>
2023-07-07 17:00:38 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,13 +5,25 @@ button.addEventListener('click',()=>{
     const input2 = document.getElementById("input2").value
     const input3 = document.getElementById("input3").value
     const input4 = document.getElementById("input4").value
-    //
+    //Calculo
     let resultado1 = parseInt(input1) + parseInt(input3)
-    console.log(resultado1)
     let resultado2 = parseInt(input2) + parseInt(input4)
-    console.log(resultado2)
     let final = "(" + resultado1 + "," + resultado2 + ")"
-    console.log(final)
-
     res.innerHTML = "Resultado: " + final
+})
+
+const button2 = document.getElementById("btn-2")
+const res2 = document.getElementById("resultado-2")
+button2.addEventListener('click', ()=>{
+    const input1 = document.getElementById("vec3_input1").value
+    const input2 = document.getElementById("vec3_input2").value
+    const input3 = document.getElementById("vec3_input3").value
+    const input4 = document.getElementById("vec3_input4").value
+    const input5 = document.getElementById("vec3_input5").value
+    const input6 = document.getElementById("vec3_input6").value
+        //Calculo
+        let resultado1 = parseInt(input1) + parseInt(input3) + parseInt(input5)
+        let resultado2 = parseInt(input2) + parseInt(input4) + parseInt(input6)
+        let final = "(" + resultado1 + "," + resultado2 + ")"
+        res2.innerHTML = "Resultado: " + final
 })
